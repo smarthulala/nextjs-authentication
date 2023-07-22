9e9dd773209649848080988d3d159300c0508b36
smarthulala <chao.long.au@gmail.com>
2023-07-22 23:47:05 +1000

    finally

--- src/app/api/users/setnewpassword/route.ts
+++ src/app/api/users/setnewpassword/route.ts
@@ -1,6 +1,7 @@
 import { connect } from '@/dbConfig/dbConfig'
 import { NextResponse, NextRequest } from 'next/server'
 import User from '@/models/userModel'
+import bcrypt from 'bcryptjs'
 
 connect()
 
@@ -20,11 +21,15 @@ export async function POST(request: NextRequest) {
         { status: 400 }
       )
     }
+    console.log('old password is ', user.password)
+
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     user.forgotPasswordToken = undefined
     user.forgotPasswordTokenExpiry = undefined
-    user.password = password
+    user.password = hashedPassword
 
+    console.log('new password is ', user.password)
     await user.save()
 
     return NextResponse.json({
